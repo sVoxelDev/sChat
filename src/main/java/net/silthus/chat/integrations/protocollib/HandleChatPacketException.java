fcb5bc184b48facc9fcb0cc56a7e7df38f3ea814
Reichenbach, Michael <michael.reichenbach@interhyp.de>
2021-11-19 07:15:22 +0100

    fix(packets): bungee chat packets not handled properly
    Closes #47

---
+++ src/main/java/net/silthus/chat/integrations/protocollib/HandleChatPacketException.java
@@ -0,0 +1,10 @@
+package net.silthus.chat.integrations.protocollib;
+
+import net.silthus.chat.SChatException;
+
+public class HandleChatPacketException extends SChatException {
+
+    public HandleChatPacketException(String message) {
+        super(message);
+    }
+}
