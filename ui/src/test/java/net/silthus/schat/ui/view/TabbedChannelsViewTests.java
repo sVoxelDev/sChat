95ba002db0509bfe58d495a2588e3b0592ee1120
Silthus <michael@reichenbach.in>
2022-01-25 05:38:50 +0100

    Revert "update"
    This reverts commit 17c51813efd61f759ba68bfd04de0fc956e5c2ac.

--- ui/src/test/java/net/silthus/schat/ui/view/TabbedChannelsViewTests.java
+++ ui/src/test/java/net/silthus/schat/ui/view/TabbedChannelsViewTests.java
@@ -1,25 +1,20 @@
 /*
- * This file is part of sChat, licensed under the MIT License.
+ * sChat, a Supercharged Minecraft Chat Plugin
  * Copyright (C) Silthus <https://www.github.com/silthus>
  * Copyright (C) sChat team and contributors
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package net.silthus.schat.ui.view;
@@ -57,7 +52,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TabbedChannelsViewTests {
 
-    private static final @NotNull MiniMessage COMPONENT_SERIALIZER = MiniMessage.miniMessage();
+    private static final @NotNull MiniMessage COMPONENT_SERIALIZER = MiniMessage.get();
     private Chatter chatter;
     private View view;
 
@@ -190,7 +185,7 @@ class TabbedChannelsViewTests {
 
                 @Test
                 void uses_custom_format() {
-                    assertViewRenders("| <green><underlined>test</underlined></green> |");
+                    assertViewRenders("| <green><underlined>test</green></underlined> |");
                 }
             }
         }
@@ -206,7 +201,7 @@ class TabbedChannelsViewTests {
 
         @Test
         void renders_both_seperated_by_a_divider() {
-            assertViewRenders("| <click:run_command:\"/channel join one\">one</click> | <click:run_command:\"/channel join two\">two</click> |");
+            assertViewRenders("| one | two |");
         }
 
         @Nested class with_different_priorities {
@@ -253,7 +248,7 @@ class TabbedChannelsViewTests {
                 No Source!
                 Player: Hey
                 Player2: Hello
-                | <green><underlined><click:run_command:"/channel join zzz">zzz</click></underlined></green> | <click:run_command:"/channel join aaa">aaa</click> |""");
+                | <underlined>zzz</underlined> | aaa |""");
         }
     }
 
