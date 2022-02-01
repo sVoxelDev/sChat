264a70d0742219ebc2768021e33230e7e3813908
Silthus <michael@reichenbach.in>
2022-02-01 06:14:01 +0100

    feat: add features to channels

---
+++ core/src/main/java/net/silthus/schat/event/Cancellable.java
@@ -0,0 +1,71 @@
+/*
+ * sChat, a Supercharged Minecraft Chat Plugin
+ * Copyright (C) Silthus <https://www.github.com/silthus>
+ * Copyright (C) sChat team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.silthus.schat.event;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents an event that can be cancelled.
+ *
+ * @since next
+ */
+public interface Cancellable {
+
+    /**
+     * Gets an {@link AtomicBoolean} holding the cancellation state of the event.
+     *
+     * @return the cancellation
+     * @since next
+     */
+    @NotNull AtomicBoolean cancellationState();
+
+    /**
+     * Returns true if the event is currently cancelled.
+     *
+     * @return if the event is cancelled
+     * @since next
+     */
+    default boolean isCancelled() {
+        return cancellationState().get();
+    }
+
+    /**
+     * Returns true if the event is not currently cancelled.
+     *
+     * @return if the event is not cancelled
+     * @since next
+     */
+    default boolean isNotCancelled() {
+        return !isCancelled();
+    }
+
+    /**
+     * Sets the cancellation state of the event.
+     *
+     * @param cancelled the new state
+     * @return the previous state
+     * @since next
+     */
+    default boolean cancelled(boolean cancelled) {
+        return cancellationState().getAndSet(cancelled);
+    }
+
+}
--- core/src/test/java/net/silthus/schat/channel/ChannelTests.java
+++ core/src/test/java/net/silthus/schat/channel/ChannelTests.java
@@ -1,30 +1,28 @@
 /*
- * This file is part of sChat, licensed under the MIT License.
+ * sChat, a Supercharged Minecraft Chat Plugin
  * Copyright (C) Silthus <https://www.github.com/silthus>
  * Copyright (C) sChat team and contributors
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package net.silthus.schat.channel;
 
+import lombok.Getter;
+import lombok.experimental.Accessors;
 import net.kyori.adventure.text.TextComponent;
+import net.silthus.schat.message.Message;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -37,7 +35,9 @@ import static net.silthus.schat.channel.Channel.DISPLAY_NAME;
 import static net.silthus.schat.channel.Channel.createChannel;
 import static net.silthus.schat.channel.ChannelHelper.channelWith;
 import static net.silthus.schat.channel.ChannelHelper.randomChannel;
+import static net.silthus.schat.channel.Feature.feature;
 import static net.silthus.schat.checks.CanJoinChannelCheck.CAN_JOIN_CHANNEL;
+import static net.silthus.schat.message.MessageHelper.randomMessage;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -81,6 +81,13 @@ class ChannelTests {
         assertThat(channel.getTargets()).isEmpty();
     }
 
+    @Test
+    void given_channel_with_same_key_then_channels_are_equal() {
+        Channel channel1 = createChannel("test");
+        Channel channel2 = createChannel("test");
+        assertThat(channel1).isEqualTo(channel2);
+    }
+
     @Nested class given_no_display_name {
         @Test
         void uses_key_as_display_name() {
@@ -121,10 +128,63 @@ class ChannelTests {
         }
     }
 
-    @Test
-    void given_channel_with_same_key_then_channels_are_equal() {
-        Channel channel1 = createChannel("test");
-        Channel channel2 = createChannel("test");
-        assertThat(channel1).isEqualTo(channel2);
+    @Nested class Features {
+
+        @Test
+        void given_no_feature_returns_empty() {
+            assertThat(channel.getFeature(MockFeature.MOCK_FEATURE)).isEmpty();
+        }
+
+        @Nested class given_mock_feature {
+            private Channel channel;
+
+            @BeforeEach
+            void setUp() {
+                channel = Channel.channel("test").withFeature(MockFeature.MOCK_FEATURE).create();
+            }
+
+            private MockFeature getFeature() {
+                return channel.getFeature(MockFeature.MOCK_FEATURE).orElseThrow();
+            }
+
+            @Test
+            void channel_feature_is_initialized() {
+                assertThat(getFeature().initializeCalled).isTrue();
+            }
+
+            @Test
+            void sendMessage_calls_feature() {
+                final Message message = randomMessage();
+                channel.sendMessage(message);
+                assertThat(getFeature().onMessageCalled).isTrue();
+                assertThat(getFeature().lastMessage).isEqualTo(message);
+            }
+        }
+
+        @Getter
+        @Accessors(fluent = true)
+        private static final class MockFeature implements Feature {
+            static final Feature.Type<MockFeature> MOCK_FEATURE = feature(MockFeature.class, MockFeature::new);
+
+            private final Channel channel;
+            public boolean initializeCalled = false;
+            public boolean onMessageCalled = false;
+            public Message lastMessage;
+
+            private MockFeature(Channel channel) {
+                this.channel = channel;
+            }
+
+            @Override
+            public void initialize() {
+                initializeCalled = true;
+            }
+
+            @Override
+            public void onMessage(Message message) {
+                onMessageCalled = true;
+                lastMessage = message;
+            }
+        }
     }
 }
