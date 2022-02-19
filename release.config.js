5de7c17a91e175863e4679e7dbb8e1db17fc350f
Silthus <michael@reichenbach.in>
2022-02-19 21:04:30 +0100

    fix: invalid if statement for prerelease channel

--- release.config.js
+++ release.config.js
@@ -13,90 +13,88 @@ const config = {
     ],
 }
 
-if (config.branches.some(it => it === branch || it.name === branch)) {
-    if (it.prerelease) {
-        config.plugins.push(
-            ["@google/semantic-release-replace-plugin", {
-                "replacements": [
-                    {
-                        "files": ["gradle.properties"],
-                        "from": "version = .*",
-                        "to": "version = ${nextRelease.version}",
-                        "results": [
-                            {
-                                "file": "gradle.properties",
-                                "hasChanged": true,
-                                "numMatches": 1,
-                                "numReplacements": 1
-                            }
-                        ],
-                        "countMatches": true
-                    }
-                ]
-            }],
-            ['@semantic-release/git', {
-                assets: [
-                    "gradle.properties"
-                ]
-            }],
-            ["@semantic-release/exec", {
-                "publishCmd": "./gradlew -PforceSign=true publish --no-daemon"
-            }],
-            ["@semantic-release/github", {
-                "assets": [
-                    {
-                        "path": "{bukkit,velocity,bungeecord}/build/libs/*!(sources|javadoc|text-fixtures).jar"
-                    }
-                ]
-            }]
-        )
-    } else {
-        config.plugins.push('@semantic-release/changelog',
-            ["@google/semantic-release-replace-plugin", {
-                "replacements": [
-                    {
-                        "files": ["gradle.properties"],
-                        "from": "version = .*",
-                        "to": "version = ${nextRelease.version}",
-                        "results": [
-                            {
-                                "file": "gradle.properties",
-                                "hasChanged": true,
-                                "numMatches": 1,
-                                "numReplacements": 1
-                            }
-                        ],
-                        "countMatches": true
-                    },
-                    {
-                        "files": [
-                            "**/src/main/java/**/*.java"
-                        ],
-                        "from": "@since next",
-                        "to": "@since ${nextRelease.version}",
-                        "countMatches": true
-                    }
-                ]
-            }],
-            ['@semantic-release/git', {
-                assets: [
-                    "**/src/main/java/**/*.java",
-                    "gradle.properties",
-                    "CHANGELOG.md"
-                ]
-            }],
-            ["@semantic-release/exec", {
-                "publishCmd": "./gradlew -PforceSign=true publish closeAndReleaseSonatypeStagingRepository --no-daemon"
-            }],
-            ["@semantic-release/github", {
-                "assets": [
-                    {
-                        "path": "{bukkit,velocity,bungeecord}/build/libs/*!(sources|javadoc|text-fixtures).jar"
-                    }
-                ]
-            }]
-        )
-    }
+if (config.branches.some(it => it === branch || (it.name === branch && it.prerelease))) {
+    config.plugins.push(
+        ["@google/semantic-release-replace-plugin", {
+            "replacements": [
+                {
+                    "files": ["gradle.properties"],
+                    "from": "version = .*",
+                    "to": "version = ${nextRelease.version}",
+                    "results": [
+                        {
+                            "file": "gradle.properties",
+                            "hasChanged": true,
+                            "numMatches": 1,
+                            "numReplacements": 1
+                        }
+                    ],
+                    "countMatches": true
+                }
+            ]
+        }],
+        ['@semantic-release/git', {
+            assets: [
+                "gradle.properties"
+            ]
+        }],
+        ["@semantic-release/exec", {
+            "publishCmd": "./gradlew -PforceSign=true publish --no-daemon"
+        }],
+        ["@semantic-release/github", {
+            "assets": [
+                {
+                    "path": "{bukkit,velocity,bungeecord}/build/libs/*!(sources|javadoc|text-fixtures).jar"
+                }
+            ]
+        }]
+    )
+} else if (config.branches.some(it => it === branch || (it.name === branch && !it.prerelease))) {
+    config.plugins.push('@semantic-release/changelog',
+        ["@google/semantic-release-replace-plugin", {
+            "replacements": [
+                {
+                    "files": ["gradle.properties"],
+                    "from": "version = .*",
+                    "to": "version = ${nextRelease.version}",
+                    "results": [
+                        {
+                            "file": "gradle.properties",
+                            "hasChanged": true,
+                            "numMatches": 1,
+                            "numReplacements": 1
+                        }
+                    ],
+                    "countMatches": true
+                },
+                {
+                    "files": [
+                        "**/src/main/java/**/*.java"
+                    ],
+                    "from": "@since next",
+                    "to": "@since ${nextRelease.version}",
+                    "countMatches": true
+                }
+            ]
+        }],
+        ['@semantic-release/git', {
+            assets: [
+                "**/src/main/java/**/*.java",
+                "gradle.properties",
+                "CHANGELOG.md"
+            ]
+        }],
+        ["@semantic-release/exec", {
+            "publishCmd": "./gradlew -PforceSign=true publish closeAndReleaseSonatypeStagingRepository --no-daemon"
+        }],
+        ["@semantic-release/github", {
+            "assets": [
+                {
+                    "path": "{bukkit,velocity,bungeecord}/build/libs/*!(sources|javadoc|text-fixtures).jar"
+                }
+            ]
+        }]
+    )
 }
 
 module.exports = config
