5c040043b1872596c3969073df12d054e70987b2
Silthus <michael@reichenbach.in>
2022-02-19 21:56:59 +0100

    chore: add channels to dev and rc releases

--- release.config.js
+++ release.config.js
@@ -4,8 +4,8 @@ const branch = ref.split('/').pop();
 const config = {
     branches: [
         'main',
-        { name: "release", prerelease: "rc", channel: false },
-        { name: "next", prerelease: "SNAPSHOT", channel: false }
+        { name: "release", prerelease: "rc", channel: "rc" },
+        { name: "next", prerelease: "SNAPSHOT", channel: "dev" }
     ],
     plugins: [
         '@semantic-release/commit-analyzer',
