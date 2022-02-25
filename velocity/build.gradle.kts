182f7e5930e65533dcbef385342c8c98f0c98623
Reichenbach, Michael <michael.reichenbach@interhyp.de>
2022-02-25 15:42:23 +0100

    feat: add bstats metrics

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -10,15 +10,19 @@ dependencies {
 
     implementation(libs.velocity)
     annotationProcessor(libs.velocity)
+
+    implementation(libs.bstats.velocity)
 }
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     dependencies {
         include(dependency("net.kyori:event-api:"))
         include(dependency("net.kyori:adventure-text-minimessage:"))
+        include(dependency("org.bstats:bstats-velocity:"))
     }
 
     val lib = "net.silthus.schat.lib"
     relocate("net.kyori.event", "$lib.kyori.event")
     relocate("net.kyori.adventure.text.minimessage", "$lib.kyori.adventure.text.minimessage")
+    relocate("org.bstats", "$lib.bstats")
 }
